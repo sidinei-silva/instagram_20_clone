1bb992d6e31d13611a9eeef85a09f4ff77af06c0
Sidinei Silva <sidinei.silva02@gmail.com>
2021-05-14 20:07:46 -0300

    Incluindo app bar

---
+++ lib/core/app_icons.dart
@@ -0,0 +1,5 @@
+class AppIcons {
+  static String get addFilled => "assets/icons/icon_add_filled.svg";
+  static String get heart => "assets/icons/icon_heart.svg";
+  static String get share => "assets/icons/icon_share.svg";
+}
